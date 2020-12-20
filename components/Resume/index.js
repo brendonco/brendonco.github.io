0becac50063229ec15f33dd303d1e4ce189729ac
Brendon Co <brendonco@Brendons-MacBook-Pro.local>
2020-12-20 12:20:05 +0800

    Added A4 page margin

--- components/Resume/index.js
+++ components/Resume/index.js
@@ -1,13 +1,14 @@
 import Header from '../Header';
 import Contact from '../Contact';
 import Skills from '../Skills';
+import { ResumeWrapper } from './resume-styled';
 
 const Resume = ({ profile }) => (
-    <>
+    <ResumeWrapper>
         <Header profile={profile} />
         <Contact profile={profile}/>
         <Skills profile={profile} />
-    </>
+    </ResumeWrapper>
 );
 
 export default Resume;
---
+++ components/Resume/resume-styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ResumeWrapper = styled.div`
+    width: 8.3in;
+    height: 11.7in;
+    padding: 1em;
+    border: 1px solid black;
+`;
