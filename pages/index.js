451253abd5e8e586ce079fba7f4e0f0b6c1d7457
Brendon Co <brendonco@hotmail.com>
2021-12-15 16:45:06 +0800

    User ssr props for loading profile

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Resume from '../components/Resume';
-import profile from '../data/profile';
 
-export async function getServerSideProps({ req, res }) {
+export async function getServerSideProps({ res }) {
     res.setHeader('Cache-Control', 'public, s-maxage=2592000, stale-while-revalidate=59');
 
+    const { default: profile } = await import('../data/profile.js');
+
     return {
-        props: {}
+        props: { profile }
     };
 }
 
-export default function Index({ time }) {
+export default function Index({ profile }) {
     return <Resume profile={profile} />;
 }
