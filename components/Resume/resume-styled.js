b9ef73f7eee60fba4e047186e6e40ac3dbda0ee0
Brendon Co <brendonco@hotmail.com>
2021-12-15 16:11:12 +0800

    Cleanup and added fadein transition

--- components/Resume/resume-styled.js
+++ components/Resume/resume-styled.js
@@ -3,5 +3,18 @@ import styled from 'styled-components';
 export const ResumeWrapper = styled.div`
     width: 8.3in;
     height: 11.7in;
-    padding: 1em;
+    padding: 0 1em;
+
+    animation: fadeInAnimation ease 3s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+
+    @keyframes fadeInAnimation {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
 `;
