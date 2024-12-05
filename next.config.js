71c5125f2050b9b241de73a1a9a0015d24f62f28
brendonco@hotmail.com <brendonco@hotmail.com>
2024-12-05 16:47:44 +0800

    Migrated to NextJS latest

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
 module.exports = {
     images: {
-        domains: ['d2sofvawe08yqg.cloudfront.net', 'images-na.ssl-images-amazon.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'd2sofvawe08yqg.cloudfront.net',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'images-na.ssl-images-amazon.com',
+                pathname: '**'
+            }
+        ]
+    },
+    experimental: {
+        turbo: {
+            rules: {
+                '*.scss': {
+                    loaders: ['sass-loader'],
+                    as: '*.css'
+                }
+            }
+        }
     }
 };
